ded8a11af6c555cdede4d1642fd8fcb104aa9f5e
Lucas Ramos <ramoslucas.lr@gmail.com>
2022-04-02 14:36:13 -0300

    feat: add Azure Function

---
+++ tcc-iot-dashboard/IoTHubToTwins/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tcc_azure_functions
+{
+    public static class Constants
+    {
+        public const string TreatmentEquipmentInitials = "EQPTO";
+        public const string TreatmentRoomInitials = "SALA";
+
+        public static readonly List<string> TreatmentEquipmentProperties = new List<string> { "OnUse", "NumericIndicator" };
+        public static readonly List<string> TreatmentRoomProperties = new List<string> { "OnUse", "NumericIndicator" };
+
+        public static readonly List<string> Telemetries = new List<string> { "ONUSE", "NUMERICINDICATOR"};
+    }
+
+    public enum DeviceType
+    {
+        TreatmentEquipment,
+        TreatmentRoom,
+        Unknown
+    }
+}
---
+++ tcc-iot-dashboard/IoTHubToTwins/IoTHubToTwins.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Azure.Core.Pipeline;
+using Azure.DigitalTwins.Core;
+using Azure.DigitalTwins.Core.Serialization;
+using Azure.Identity;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace tcc_azure_functions
+{
+    public class IoTHubToTwins
+    {
+        private static readonly string adtInstanceUrl = Environment.GetEnvironmentVariable("ADT_SERVICE_URL");
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        [FunctionName("IoTHubToTwins")]
+
+        public async Task Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
+        {
+            if (adtInstanceUrl == null) log.LogError("\"ADT_SERVICE_URL\" não configurada");
+
+            try
+            {
+                if (eventGridEvent != null && eventGridEvent.Data != null)
+                {
+                    ManagedIdentityCredential cred = new ManagedIdentityCredential("https://digitaltwins.azure.net");
+                    DigitalTwinsClient client = new DigitalTwinsClient(new Uri(adtInstanceUrl), cred, new DigitalTwinsClientOptions { Transport = new HttpClientTransport(httpClient) });
+                    log.LogInformation(eventGridEvent.Data.ToString());
+
+                    JObject deviceMessage = (JObject)JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
+                    string deviceId = (string)deviceMessage["systemProperties"]["iothub-connection-device-id"];
+                    var deviceType = Utils.GetDeviceType(deviceId);
+                    var body = deviceMessage["body"];
+
+                    switch (deviceType)
+                    {
+                        case DeviceType.TreatmentRoom:
+                            await UpdateDigitalTwinProperty(client, deviceId, body, Constants.TreatmentRoomProperties);
+                            break;
+                        case DeviceType.TreatmentEquipment:
+                            await UpdateDigitalTwinProperty(client, deviceId, body, Constants.TreatmentEquipmentProperties);
+                            break;
+                        default:
+                            log.LogInformation("Unknown device type.");
+                            break;
+                    }
+                    log.LogInformation($"DigitalTwinUpdated");
+                }
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, $"Error in ingest function: {ex.Message}");
+            }
+        }
+
+        private async Task UpdateDigitalTwinProperty(DigitalTwinsClient client, string deviceId, JToken body, List<string> properties)
+        {
+            foreach (var property in properties)
+            {
+                await UpdateDigitalTwinProperty(client, deviceId, body, property);
+            }
+        }
+
+        private async Task UpdateDigitalTwinProperty(DigitalTwinsClient client, string deviceId, JToken body, string propertyName)
+        {
+            var propertyToken = body[propertyName];
+            if (propertyToken != null)
+            {
+                if (Constants.Telemetries.Contains(propertyName.ToUpper()))
+                {
+                    var data = new Dictionary<string, double>();
+                    data.Add(propertyName, propertyToken.Value<double>());
+                    await client.PublishTelemetryAsync(deviceId, null, JsonConvert.SerializeObject(data));
+                }
+            }
+        }
+    }
+}
